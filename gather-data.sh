0834a89c4bdfb2e54e2cf793d5f7292a5c968f52
smessie <smessie@smessie.com>
2024-10-30 16:39:48 +0100

    chore(setup): Pipelines for different datasets

---
+++ gather-data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+npx js-runner setup/pipeline-telraam-hour.ttl
+npx js-runner setup/pipeline-telraam-day.ttl
+npx js-runner setup/pipeline-telraam-week.ttl
+npx js-runner setup/pipeline-telraam-month.ttl
+npx js-runner setup/pipeline-telraam-3months.ttl
+npx js-runner setup/pipeline-telraam-6months.ttl
