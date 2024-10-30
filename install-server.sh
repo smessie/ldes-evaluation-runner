ce35236536476495f021029608f39e1281fd4713
smessie <smessie@smessie.com>
2024-10-30 15:55:07 +0100

    fix(install): Correctly load nvm installation

--- install-server.sh
+++ install-server.sh
@@ -3,7 +3,11 @@
 # wget -O - -nv --ciphers DEFAULT@SECLEVEL=1 https://www.wall2.ilabt.iminds.be/enable-nat.sh | sudo bash
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-source ~/.bashrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install 22
 
 cd client
