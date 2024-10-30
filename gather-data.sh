abb2b1d7d3588f8747de8daf8fb2660c14bffc80
smessie <smessie@smessie.com>
2024-10-30 16:57:32 +0100

    chore(setup): Move script

--- gather-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-npx js-runner setup/pipeline-telraam-hour.ttl
-npx js-runner setup/pipeline-telraam-day.ttl
-npx js-runner setup/pipeline-telraam-week.ttl
-npx js-runner setup/pipeline-telraam-month.ttl
-npx js-runner setup/pipeline-telraam-3months.ttl
-npx js-runner setup/pipeline-telraam-6months.ttl
---
+++ setup/gather-data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+npx js-runner pipeline-telraam-hour.ttl
+npx js-runner pipeline-telraam-day.ttl
+npx js-runner pipeline-telraam-week.ttl
+npx js-runner pipeline-telraam-month.ttl
+npx js-runner pipeline-telraam-3months.ttl
+npx js-runner pipeline-telraam-6months.ttl
