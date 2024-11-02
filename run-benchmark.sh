3c4e7b22c9aa426c746ad50e73e6ebfec4f02a5c
smessie <smessie@smessie.com>
2024-11-02 14:33:27 +0100

    chore(config): Synchronization of Telraam

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-paged-complete-1c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-paged-complete-1c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-paged-complete-2c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-paged-complete-2c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-paged-complete-5c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-paged-complete-5c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-paged-complete-10c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-paged-complete-10c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-paged-complete-20c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-paged-complete-20c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-paged-complete-30c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-paged-complete-30c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-paged-complete-40c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-paged-complete-40c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-paged-complete-50c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-paged-complete-50c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-paged-complete-100c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-paged-complete-100c.json $SERVER_HOSTNAME
 
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-timebased-complete-1c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-timebased-complete-1c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-timebased-complete-2c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-timebased-complete-2c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-timebased-complete-5c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-timebased-complete-5c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-timebased-complete-10c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-timebased-complete-10c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-timebased-complete-20c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-timebased-complete-20c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-timebased-complete-30c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-timebased-complete-30c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-timebased-complete-40c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-timebased-complete-40c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-timebased-complete-50c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-timebased-complete-50c.json $SERVER_HOSTNAME
-node runner /users/iesmessa/ldes-evaluation-runner/env/telraam/replication/hour-timebased-complete-100c.env /users/iesmessa/ldes-evaluation-runner/out/telraam/replication/hour-timebased-complete-100c.json $SERVER_HOSTNAME
+# Replication of Telraam benchmark
+ENV_DIR_TELRAAM_REPLICATION="/users/iesmessa/ldes-evaluation-runner/env/telraam/replication"
+OUT_DIR_TELRAAM_REPLICATION="/users/iesmessa/ldes-evaluation-runner/out/telraam/replication"
+
+for env_file in "$ENV_DIR_TELRAAM_REPLICATION"/*.env; do
+    base_name=$(basename "$env_file" .env)
+
+    out_file="$OUT_DIR_TELRAAM_REPLICATION/${base_name}.json"
+
+    node runner "$env_file" "$out_file" "$SERVER_HOSTNAME"
+done
+
+
+# Synchronization of Telraam benchmark
+ENV_DIR_TELRAAM_SYNCHRONIZATION="/users/iesmessa/ldes-evaluation-runner/env/telraam/synchronization"
+OUT_DIR_TELRAAM_SYNCHRONIZATION="/users/iesmessa/ldes-evaluation-runner/out/telraam/synchronization"
+
+for env_file in "$ENV_DIR_TELRAAM_SYNCHRONIZATION"/*.env; do
+    base_name=$(basename "$env_file" .env)
+
+    out_file="$OUT_DIR_TELRAAM_SYNCHRONIZATION/${base_name}.json"
+
+    node runner "$env_file" "$out_file" "$SERVER_HOSTNAME"
+done
