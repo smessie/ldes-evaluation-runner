07db156dc01a9b8d078d2c058f169f5640487d33
smessie <smessie@smessie.com>
2025-03-03 12:58:37 +0100

    refactor: Move Telraam benchmark to https://github.com/smessie/ldes-benchmark

--- setup/gather-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-npx js-runner pipeline-telraam-hour.ttl
-npx js-runner pipeline-telraam-day.ttl
-npx js-runner pipeline-telraam-week.ttl
-npx js-runner pipeline-telraam-month.ttl
-npx js-runner pipeline-telraam-3months.ttl
-npx js-runner pipeline-telraam-6months.ttl
