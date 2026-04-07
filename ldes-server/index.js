327d75098033f44f32b3ad7795e4cea9b84e6d90
smessie <smessie@smessie.com>
2026-04-07 14:45:53 +0200

    chore(deps): Update ingest-server and ldes-server dependencies

--- ldes-server/index.js
+++ ldes-server/index.js
@@ -6,9 +6,15 @@ async function run(argv) {
     if (process.env.DATABASE_URL) {
         variableBindings["urn:solid-server:default:db-url"] = process.env.DATABASE_URL;
     }
+    if (process.env.DATABASE_TYPE) {
+        variableBindings["urn:solid-server:default:db-type"] = process.env.DATABASE_TYPE;
+    }
     if (process.env.SERVER_HOSTNAME) {
         variableBindings["urn:solid-server:default:ldes-url"] = `http://${process.env.SERVER_HOSTNAME}:3000/ldes`;
         variableBindings["urn:solid-server:default:view-url"] = `http://${process.env.SERVER_HOSTNAME}:3000/ldes/#fragmentation`;
+    }
+    if (process.env.SERVER_NAMED_GRAPHS) {
+        variableBindings["urn:solid-server:default:named-graphs"] = process.env.SERVER_NAMED_GRAPHS;
     }
 
     // Get -c or --config value
